6b0f824cccff534d3ae3141926c589031ccc13e6
Liav <48312928+liav25@users.noreply.github.com>
2019-05-05 20:08:52 +0300

    change status bar colors

--- Feed_Activity/app/src/main/java/com/example/feed_activity/MealActivity.java
+++ Feed_Activity/app/src/main/java/com/example/feed_activity/MealActivity.java
@@ -2,7 +2,9 @@ package com.example.feed_activity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,15 +23,17 @@ public class MealActivity extends AppCompatActivity {
 
 
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.constraint_meal_page);
+        getWindow().setStatusBarColor(this.getResources().getColor(R.color.TextYellow));
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.meal_toolbar);
-        setSupportActionBar(toolbar);
         toolbar.setBackgroundColor(getResources().getColor(R.color.TextYellow));
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
