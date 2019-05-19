b5407e7c29c0d9de61b5e90c6f98c60e93b44539
Liav <48312928+liav25@users.noreply.github.com>
2019-05-19 16:29:55 +0300

    add profile pictures

--- Feed_Activity/app/src/main/java/com/example/feed_activity/Meal_Card.java
+++ Feed_Activity/app/src/main/java/com/example/feed_activity/Meal_Card.java
@@ -2,19 +2,41 @@ package com.example.feed_activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class Meal_Card extends AppCompatActivity {
 
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feed_card);
-
-
+        getImages();
 
     }
 
+    private void getImages(){
+        mImageUrls.add("Https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mNames.add("liavi");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+
+    }
 }
