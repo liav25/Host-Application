4f10751c3a83569d7c3f5c6eba1f5deea47c312c
elishevasf <50096215+elishevasf@users.noreply.github.com>
2019-05-20 09:56:18 +0300

    Changed name

--- Feed_Activity/app/src/main/java/com/example/feed_activity/MainActivity.java
+++ Feed_Activity/app/src/main/java/com/example/feed_activity/MainActivity.java
@@ -141,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init(){
-        sev.addUser("Deborah Carmi", "password", null,"Open University",
+        sev.addUser("Degbbborah Carmi", "password", null,"Open University",
                 new HashSet<String>(Arrays.asList("Hebrew", "English", "Arabic")), new HashSet<String>());
         sev.addUser("Elisheva", "password", null,"Hebrew University",
                 new HashSet<String>(Arrays.asList("Hebrew", "Arabic")), new HashSet<String>());
