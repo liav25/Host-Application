98f3afdb58f8cb2014e3838f12b53092a8f92d29
tomisalz <43648252+tomisalz@users.noreply.github.com>
2019-05-27 16:52:46 +0300

    Revert "Includes profile pics hashmap s.t. we will get less calls to server"
    This reverts commit 2108c5598bda8f595890739f0e5461597d181440.

--- Feed_Activity/app/src/main/java/com/example/hoster/MainActivity.java
+++ Feed_Activity/app/src/main/java/com/example/hoster/MainActivity.java
@@ -6,8 +6,6 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
--- Feed_Activity/app/src/main/java/com/example/hoster/edit_profile.java
+++ Feed_Activity/app/src/main/java/com/example/hoster/edit_profile.java
@@ -120,8 +120,6 @@ public class edit_profile extends AppCompatActivity {
                             Uri downloadUrl = imageUri;
                             fileUrl = downloadUrl.toString();
                             Server.getInstance().editProfilePic(downloadUrl);
-                            Server.getInstance().pics.remove(MainActivity.userId); // then it will be refreshed
-                            // in UI
                             Server.getInstance().downloadProfilePic(MainActivity.profilePicture,
                                     MainActivity.userId);
 
